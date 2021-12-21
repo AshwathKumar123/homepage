587c308b496bbe439df53d1256af40030f9d1398
Vyzic <92567671+VyzicGithub@users.noreply.github.com>
2021-12-21 11:23:14 +0530

    Added all the files
    First commit

---
+++ bookmarks.js
@@ -0,0 +1,45 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Essentials",
+    links: [
+      { name: "Gmail", url: "https://gmail.com" },
+      { name: "Teams", url: "https://teams.microsoft.com" },
+      { name: "GitHub", url: "https://github.com" },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      { name: "YouTube", url: "https://youtube.com" },
+      { name: "Twitch", url: "https://twitch.tv" },
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Discord", url: "https://discord.com/app" },
+      { name: "Spotify", url: "https://open.spotify.com" },
+    ],
+  },
+  {
+    title: "Gaming",
+    links: [
+      { name: "Steam", url: "https://store.steampowered.com" },
+      {
+        name: "Minecraft",
+        url: "https://minecraft.net",
+      },
+      { name: "Fortnite", url: "https://www.epicgames.com/fortnite/en-US/home" },
+      {
+        name: "Steam (Open Client)",
+        url: "steam://open/main",
+      },
+      { name: "Spotify (Open Client)", url: "spotify://open" },
+      { name: "Reserved 3", url: "about:blank" },
+    ],
+  },
+  {
+    title: "Random",
+    links: [
+      { name: "Reserved 4", url: "about:blank" },
+      { name: "Reserved 5", url: "about:blank" },
+    ],
+  },
+];
